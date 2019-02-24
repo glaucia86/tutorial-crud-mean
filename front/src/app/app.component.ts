949e4af50dd81aad639bf5f9f0e2366ed6038cb4
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-24 16:27:11 -0300

    Desenvolvimento da lógica do Loading Progress Bar na aplicação.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { SlimLoadingBarService } from 'ng2-slim-loading-bar';
+import { NavigationCancel,
+          NavigationEnd,
+          NavigationError,
+          NavigationStart,
+          Event,
+          Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'front';
+
+  constructor(private loadingBarService: SlimLoadingBarService, private router: Router) {
+    this.router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  /**
+   * @param event Método responsável por lidar com as ações do 'loading Bar Progress' da Navegação.
+   */
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this.loadingBarService.start();
+    }
+    if (event instanceof NavigationEnd) {
+      this.loadingBarService.complete();
+    }
+    if (event instanceof NavigationCancel) {
+      this.loadingBarService.stop();
+    }
+    if (event instanceof NavigationError) {
+      this.loadingBarService.stop();
+    }
+  }
 }
+
