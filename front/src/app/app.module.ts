03d5e6e01651317672ca1404e7cd7bc9c5cb3dbe
glaucia86 <glaucia_lemos86@hotmail.com>
2019-02-24 21:20:24 -0300

    Desenvolvimento do form na aplicação.

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { FuncGetComponent } from './func-get/func-get.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule
+    SlimLoadingBarModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
