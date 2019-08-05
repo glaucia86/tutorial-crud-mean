36a9368e69bcbf38c5b3b06f29b93fab8d4e9cdd
glaucia86 <glaucia_lemos86@hotmail.com>
2019-08-05 19:11:33 -0300

    Desenvolvimento da aula - 26.

--- api/src/controllers/funcionario.controller.js
+++ api/src/controllers/funcionario.controller.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /**
  * Arquivo: src/controllers/funcionario.controller.js
  * Descrição: arquivo responsável pelo CRUD da classe 'Funcionário'
@@ -18,75 +17,30 @@ exports.create = async (req, res) => {
 };
 
 // ==> Método responsável por selecionar todos os Funcionários:
-exports.findAll = (req, res) => {
-  Funcionario.find()
-    .then((funcionarios) => {
-      res.status(200).send(funcionarios);
-    }).catch((err) => {
-      res.status(500).send({ message: 'Erro ao selecionar todos(as) os(as) Funcionários(as)' || err.message });
-    });
+exports.findAll = async (req, res) => {
+  const funcionarios = await Funcionario.find({});
+  res.status(200).send(funcionarios);
 };
 
 // ==> Método responsável por selecionar 'Funcionario' pelo 'Id':
-exports.findById = (req, res) => {
-  Funcionario.findById(req.params.id)
-    .then((funcionario) => {
-      if (!funcionario) {
-        return res.status(404).send({ message: `Funcionário(a) não encontrado(a) ${req.params.id}` });
-      }
-
-      res.status(200).send(funcionario);
-    }).catch((err) => {
-      if (err.kind === 'ObjectId') {
-        return res.status(400).send({ message: `Id do Funcionário(a) não encontrado(a) ${req.params.id}` });
-      }
-
-      res.status(500).send({ message: 'Erro ao selecionar os(as) Funcionários(as) || err.message' });
-    });
+exports.findById = async (req, res) => {
+  const funcionario = await Funcionario.findById(req.params.id);
+  res.status(200).send(funcionario);
 };
 
 // ==> Método responsável por atualizar 'Funcionário' pelo 'Id':
-exports.update = (req, res) => {
-  // Validar os campos
-  if (!req.body.nomeFuncionario) {
-    return res.status(400).send({ message: 'Os campos não podem ser vazios! ' });
+exports.update = async (req, res) => {
+  // => Validação de campos vazios:
+  if (!req.body.nomeFuncionario || !req.body.cargo || !req.body.numeroIdentificador) {
+    return res.status(400).send({ message: 'Os campos não podem ser vazios.' });
   }
 
-  // Encontrar 'Id' do Funcionário e depois atualizar os dados via 'request':
-  Funcionario.findByIdAndUpdate(req.params.id, {
-    nomeFuncionario: req.body.nomeFuncionario,
-    cargo: req.body.cargo,
-    numeroIdentificador: req.body.numeroIdentificador,
-  }, { new: true })
-    .then((funcionario) => {
-      if (!funcionario) {
-        res.status(404).send({ message: `Funcionário(a) não encontrado(a) ${req.params.id}` });
-      }
-
-      res.status(200).send({ message: 'Funcionário(a) atualizado(a) com sucesso!', funcionario });
-    }).catch((err) => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({ message: `Erro ao encontrar o Id do Funcionário(a) ${req.params.id}` });
-      }
-
-      res.status(500).send({ message: `Erro ao atualizar os dados do(a) Funcionário(a) ${req.params.id}` });
-    });
+  const funcionario = await Funcionario.findByIdAndUpdate(req.params.id, req.body);
+  return res.status(200).send({ message: 'Funcionário(a) atualizado(a) com sucesso', funcionario });
 };
 
 // ==> Método responsável por excluir 'Funcionário' pelo 'Id':
-exports.delete = (req, res) => {
-  Funcionario.findByIdAndDelete(req.params.id)
-    .then((funcionario) => {
-      if (!funcionario) {
-        return res.status(404).send({ message: `Id do Funcionário(a) não encontrado(a) ${req.params.id}` });
-      }
-
-      res.status(200).send({ message: 'Funcionário(a) excluído com sucesso!', funcionario });
-    }).catch((err) => {
-      if (err.kind === 'ObjectId') {
-        return res.status(404).send({ message: `Id do Funcionário(a) não encontrado ${req.params.id}` });
-      }
-
-      return res.status(500).send({ message: `Erro ao excluir Funcionário(a) ${req.params.id}` });
-    });
+exports.delete = async (req, res) => {
+  const funcionario = await Funcionario.findByIdAndDelete(req.params.id);
+  return res.status(200).send({ message: 'Funcionário(a) excluído com sucesso!', funcionario });
 };
