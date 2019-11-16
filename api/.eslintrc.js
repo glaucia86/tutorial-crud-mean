36b438e030974daa46e33e837e1fda5a4bda8d42
Kusther <luciano.mamonas98@gmail.com>
2019-11-16 00:40:24 -0300

    Remove body-parser dependencie, Because comes by default in express.

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-      "no-console": "off",
-      "max-len": "off",
-      "prefer-destructuring": "off",
-      "consistent-return": "off"
-    }
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "extends": "airbnb-base",
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "rules": {
+    "no-console": "off",
+    "max-len": "off",
+    "prefer-destructuring": "off",
+    "consistent-return": "off"
+  }
 };
--- api/src/app.js
+++ api/src/app.js
@@ -5,37 +5,39 @@
  * Author: Glaucia Lemos
  */
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
 // Importar o arquivo: 'database.js'
-const localDatabase = require('./config/database'); // ==> persistencia de maneira local: MongoDb
+const localDatabase = require("./config/database"); // ==> persistencia de maneira local: MongoDb
 // const databaseCosmosDb = require('./config/databaseCosmosDb'); // ==> persistencia de maneira nuvem: CosmosDb
 
 mongoose.Promise = global.Promise;
 
 // ==> Conexão com a Base de Dados:
-mongoose.connect(localDatabase.local.localUrl, { useNewUrlParser: true }).then(() => {
-  console.log('A Base de dados foi conectada com sucesso!');
-}, (err) => {
-  console.log(`Erro ao conectar com a base de Dados...: ${err}`);
-  process.exit();
-});
+mongoose.connect(localDatabase.local.localUrl, { useNewUrlParser: true }).then(
+  () => {
+    console.log("A Base de dados foi conectada com sucesso!");
+  },
+  err => {
+    console.log(`Erro ao conectar com a base de Dados...: ${err}`);
+    process.exit();
+  }
+);
 
 // ==> Rotas
-const funcionarioRoute = require('./routes/funcionario.routes');
+const funcionarioRoute = require("./routes/funcionario.routes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.json({ type: "application/vnd.api+json" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 app.use(cors());
 
-app.use('/api/', funcionarioRoute);
+app.use("/api/", funcionarioRoute);
 
 module.exports = app;
